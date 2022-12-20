3d181a43305195a219d0812c120841a7404f4ddc
Chandan Sarkar <sarkar1986.chandan@gmail.com>
2022-12-21 00:52:36 +0100

    chore: Implement config base api

--- include/cfg.hpp
+++ include/cfg.hpp
@@ -5,32 +5,35 @@
 #include <string>
 #include <queue>
 #include <filesystem>
+#include <memory>
 
 #include <boost/optional.hpp>
 #include <yaml-cpp/yaml.h>
 
 namespace cfg
 {
-    /// @brief Implements a wrapper over yaml-cpp library to implement a utility method to fetch configurations from
-    /// file.
+    /// @brief Implements a wrapper over yaml-cpp to implement a utility method to fetch config values from file.
+    /// A config file could be arbitrarily large, hence we initialize the YAML::Node tree structure onto heap.
     class ConfigBase
     {
-        YAML::Node _config;
-        std::string _default_delimiter;
+        std::unique_ptr<YAML::Node> _root; // Root node of the YAML config tree
+        std::string _delimeter;            // Separator for key segments of hierarchical config items
+        std::filesystem::path _path;       // Base path for config file
 
         /// @brief Parses hierarchical key string into a queue of identifiers for individual levels in the
-        /// hierarchical configuration structure. This is done depending on a delimiter separating the combined
+        /// hierarchical config structure. This is done depending on a delimiter separating the combined
         /// keys in a single string.
+        /// @example "road_color_saturation" => ["road" -> "color" -> "saturation"]
         std::queue<std::string> parseConfig(std::string const &_key) const
         {
             std::queue<std::string> buffer;
             size_t start = 0;
-            size_t end = _key.find(_default_delimiter);
+            size_t end = _key.find(_delimeter);
             while (end != std::string::npos)
             {
                 buffer.push(_key.substr(/*pos*/ start, /*count*/ end - start));
-                start = (end + _default_delimiter.length());
-                end = _key.find(_default_delimiter, start);
+                start = (end + _delimeter.length());
+                end = _key.find(_delimeter, start);
             }
             buffer.push(_key.substr(start, end));
             return buffer;
@@ -39,8 +42,8 @@ namespace cfg
         /// @brief Recursively fetches keys from hierarchical yaml nodes using separated key segments.
         /// Yaml-cpp supports a number of node types such as null, scalar, sequence maps etc. A map denotes
         /// existence of a single key-value pair or existence of nested key-value pairs. When we exhaust all
-        /// key segments and simultaneously currently traversed node is not a map, it means we have reached a
-        /// node which denotes a scalar, a sequence or absence of a value, at which point we return the node.
+        /// key segments and currently traversed node is not a map, it means we have reached a node which
+        /// denotes a scalar, a sequence or null value, at which point we return the node.
         YAML::Node fetch(YAML::Node node, std::queue<std::string> &_segments) const
         {
             if (!node.IsMap() && _segments.empty())
@@ -53,48 +56,91 @@ namespace cfg
         }
 
     public:
-        /// @brief Constructs ConfigBase from a YAML configuration file and a default delimiter for hierarchical
-        /// configurations.
-        ConfigBase(std::filesystem::path const &_cfg_path, std::string const &_def_delim = "_")
-            : _default_delimiter(_def_delim)
+        /// @brief ConfigBase constructor
+        explicit ConfigBase(std::filesystem::path const &_cfg_path)
+            : _delimeter("_"), _path(_cfg_path)
         {
-            try
-            {
-                _config = YAML::LoadFile(_cfg_path);
-            }
-            catch (const std::exception &e)
-            {
-                std::cerr << e.what() << "\n";
-                throw std::runtime_error("invalid config file");
-            }
+            _root = std::make_unique<YAML::Node>();
+            *_root = YAML::LoadFile(_cfg_path);
         }
+        ConfigBase() = delete;
         ~ConfigBase() {}
 
-        /// @brief Implements accessor for config values against specified key
+        ConfigBase(ConfigBase const &_other)
+            : _delimeter(_other._delimeter), _path(_other._path)
+        {
+            _root.release();
+            _root = std::make_unique<YAML::Node>();
+            *_root = YAML::LoadFile(_path);
+        }
+
+        ConfigBase operator=(ConfigBase const &_other)
+        {
+            if (*this == _other)
+            {
+                return *this;
+            }
+            _delimeter = _other._delimeter;
+            _path = _other._path;
+            _root.release();
+            _root = std::make_unique<YAML::Node>();
+            *_root = YAML::LoadFile(_path);
+            return *this;
+        }
+
+        inline bool operator==(cfg::ConfigBase const &_other) const
+        {
+            return _delimeter == _other._delimeter && _path == _other._path && _root->Type() == _other._root->Type();
+        }
+
+        inline bool operator!=(cfg::ConfigBase const &_other) const
+        {
+            return !(*this == _other);
+        }
+
+        /// @brief Common accessor api for config values against specified key.
         /// @tparam T configuration value type
-        /// @param key configuration key
-        /// @return configuration value
+        /// @param key config key
+        /// @return optional config value
         template <typename T>
         boost::optional<T> Get(std::string const &key) const
         {
-            if (key.find(_default_delimiter) == std::string::npos)
+            if (key.find(_delimeter) == std::string::npos)
             {
-                if (!_config[key].IsDefined())
+                if (!(*_root)[key].IsDefined() || (*_root).IsNull())
                 {
                     return boost::none;
                 }
-                return _config[key].as<T>();
+                return (*_root)[key].as<T>();
             }
             std::queue<std::string> _segments = parseConfig(key);
+            // At this point key segments must not be empty
             assert(!_segments.empty());
-            YAML::Node val = fetch(_config, _segments);
-            if (!val.IsDefined())
+            YAML::Node val = fetch((*_root), _segments);
+            if (!val.IsDefined() || val.IsNull())
             {
                 return boost::none;
             }
             return val.as<T>();
         }
     };
+
+    /// @brief Instantiates cfg::ConfigBase while encapsulating the error handling.
+    /// @param _abs_path absolute path to the config file
+    /// @return optional cfg::ConfigBase
+    boost::optional<cfg::ConfigBase> GetConfig_From(std::filesystem::path const &_abs_path)
+    {
+        try
+        {
+            cfg::ConfigBase base = cfg::ConfigBase(_abs_path);
+            return base;
+        }
+        catch (YAML::BadFile const &bf)
+        {
+            std::cerr << bf.what() << '\n';
+            return boost::none;
+        }
+    }
 } // namespace cfg
 
 #endif // CFG_HPP
--- tests/cfg_test.cpp
+++ tests/cfg_test.cpp
@@ -12,33 +12,28 @@ const std::filesystem::path t_config_path = "../../tests/test_config.yaml";
 
 TEST_CASE("config must be read from valid config file")
 {
-    try
-    {
-        cfg::ConfigBase cfg_base = std::filesystem::absolute("invalid path");
-    }
-    catch (std::runtime_error const &re)
-    {
-        REQUIRE(static_cast<std::string>(re.what()) == "invalid config file");
-    }
+    const std::filesystem::path invalid_path = "invalid";
+    REQUIRE_FALSE(cfg::GetConfig_From(invalid_path).has_value());
+    REQUIRE(cfg::GetConfig_From(std::filesystem::absolute(t_config_path)).has_value());
 }
 
 TEST_CASE("config cannot be read using empty or invalid key")
 {
-    cfg::ConfigBase cfg_base = {std::filesystem::absolute(t_config_path)};
+    const cfg::ConfigBase base = cfg::GetConfig_From(std::filesystem::absolute(t_config_path)).value();
     const std::string EMPTY_KEY = "";
     const std::string FIRST_LEVEL_INVALID_KEY = "invalid";
     const std::string SECOND_LEVEL_INVALID_KEY = "attributes_invalid";
     const std::string THIRD_LEVEL_INVALID_KEY = "road_color_invalid";
     const std::string ALL_LEVELS_INVALID = "all_levels_invalid";
-    REQUIRE_FALSE(cfg_base.Get<double>("").has_value());
-    REQUIRE_FALSE(cfg_base.Get<cfg::Vec3i>(SECOND_LEVEL_INVALID_KEY).has_value());
-    REQUIRE_FALSE(cfg_base.Get<double>(THIRD_LEVEL_INVALID_KEY).has_value());
-    REQUIRE_FALSE(cfg_base.Get<int>(ALL_LEVELS_INVALID).has_value());
+    REQUIRE_FALSE(base.Get<double>(EMPTY_KEY).has_value());
+    REQUIRE_FALSE(base.Get<cfg::Vec3i>(SECOND_LEVEL_INVALID_KEY).has_value());
+    REQUIRE_FALSE(base.Get<double>(THIRD_LEVEL_INVALID_KEY).has_value());
+    REQUIRE_FALSE(base.Get<int>(ALL_LEVELS_INVALID).has_value());
 }
 
-TEST_CASE("Configurations can be read from valid config file")
+TEST_CASE("config can be read from valid config file and keys")
 {
-    cfg::ConfigBase cfg_base = {std::filesystem::absolute(t_config_path)};
+    const cfg::ConfigBase base = cfg::GetConfig_From(std::filesystem::absolute(t_config_path)).value();
     // Define test key configurations
     const std::string PI = "pi";
     const std::string ATTRIBUTES_NAME = "attributes_name";
@@ -71,33 +66,33 @@ TEST_CASE("Configurations can be read from valid config file")
     std::string e_attr_name = "some name";
     bool e_attr_debug = true;
     // Validate simple non-nested configuration
-    REQUIRE(cfg_base.Get<double>(PI).has_value());
-    REQUIRE(cfg_base.Get<double>(PI).value() == e_val_map[PI]);
+    REQUIRE(base.Get<double>(PI).has_value());
+    REQUIRE(base.Get<double>(PI).value() == e_val_map[PI]);
     // Validate single nested configuration
-    REQUIRE(cfg_base.Get<std::string>(ATTRIBUTES_NAME).has_value());
-    REQUIRE(cfg_base.Get<std::string>(ATTRIBUTES_NAME).value() == e_attr_name);
-    REQUIRE(cfg_base.Get<bool>(ATTRIBUTES_DEBUG).has_value());
-    REQUIRE(cfg_base.Get<bool>(ATTRIBUTES_DEBUG).value() == e_attr_debug);
+    REQUIRE(base.Get<std::string>(ATTRIBUTES_NAME).has_value());
+    REQUIRE(base.Get<std::string>(ATTRIBUTES_NAME).value() == e_attr_name);
+    REQUIRE(base.Get<bool>(ATTRIBUTES_DEBUG).has_value());
+    REQUIRE(base.Get<bool>(ATTRIBUTES_DEBUG).value() == e_attr_debug);
     // Validate double nested configuration
-    REQUIRE(cfg_base.Get<double>(ROAD_DIMS_LENGTH).has_value());
-    REQUIRE(cfg_base.Get<double>(ROAD_DIMS_LENGTH).value() == e_val_map[ROAD_DIMS_LENGTH]);
-    REQUIRE(cfg_base.Get<double>(ROAD_DIMS_WIDTH).has_value());
-    REQUIRE(cfg_base.Get<double>(ROAD_DIMS_WIDTH).value() == e_val_map[ROAD_DIMS_WIDTH]);
-    REQUIRE(cfg_base.Get<double>(ROAD_DIMS_HEIGHT).has_value());
-    REQUIRE(cfg_base.Get<double>(ROAD_DIMS_HEIGHT).value() == e_val_map[ROAD_DIMS_HEIGHT]);
-    REQUIRE(cfg_base.Get<double>(ROAD_COLOR_HUE).has_value());
-    REQUIRE(cfg_base.Get<double>(ROAD_COLOR_HUE).value());
-    REQUIRE(cfg_base.Get<double>(ROAD_COLOR_SATURATION).has_value());
-    REQUIRE(cfg_base.Get<double>(ROAD_COLOR_SATURATION).value() == e_val_map[ROAD_COLOR_SATURATION]);
-    REQUIRE(cfg_base.Get<double>(ROAD_COLOR_VALUE).has_value());
-    REQUIRE(cfg_base.Get<double>(ROAD_COLOR_VALUE).value() == e_val_map[ROAD_COLOR_VALUE]);
+    REQUIRE(base.Get<double>(ROAD_DIMS_LENGTH).has_value());
+    REQUIRE(base.Get<double>(ROAD_DIMS_LENGTH).value() == e_val_map[ROAD_DIMS_LENGTH]);
+    REQUIRE(base.Get<double>(ROAD_DIMS_WIDTH).has_value());
+    REQUIRE(base.Get<double>(ROAD_DIMS_WIDTH).value() == e_val_map[ROAD_DIMS_WIDTH]);
+    REQUIRE(base.Get<double>(ROAD_DIMS_HEIGHT).has_value());
+    REQUIRE(base.Get<double>(ROAD_DIMS_HEIGHT).value() == e_val_map[ROAD_DIMS_HEIGHT]);
+    REQUIRE(base.Get<double>(ROAD_COLOR_HUE).has_value());
+    REQUIRE(base.Get<double>(ROAD_COLOR_HUE).value());
+    REQUIRE(base.Get<double>(ROAD_COLOR_SATURATION).has_value());
+    REQUIRE(base.Get<double>(ROAD_COLOR_SATURATION).value() == e_val_map[ROAD_COLOR_SATURATION]);
+    REQUIRE(base.Get<double>(ROAD_COLOR_VALUE).has_value());
+    REQUIRE(base.Get<double>(ROAD_COLOR_VALUE).value() == e_val_map[ROAD_COLOR_VALUE]);
     // Validate vector configuration
-    REQUIRE(cfg_base.Get<cfg::Vec3d>(ATTRIBUTES_POINT).has_value());
-    REQUIRE(cfg_base.Get<cfg::Vec3d>(ATTRIBUTES_POINT).value() == e_point_xyz);
-    REQUIRE(cfg_base.Get<cfg::Vec3i>(ATTRIBUTES_RGB).has_value());
-    REQUIRE(cfg_base.Get<cfg::Vec3i>(ATTRIBUTES_RGB).value() == e_rgb);
-    REQUIRE(cfg_base.Get<cfg::Vec3b>(ATTRIBUTES_FLAGS).has_value());
-    REQUIRE(cfg_base.Get<cfg::Vec3b>(ATTRIBUTES_FLAGS).value() == e_flags);
-    REQUIRE(cfg_base.Get<cfg::Vec3Str>(ATTRIBUTES_NAMES).has_value());
-    REQUIRE(cfg_base.Get<cfg::Vec3Str>(ATTRIBUTES_NAMES).value() == e_names);
+    REQUIRE(base.Get<cfg::Vec3d>(ATTRIBUTES_POINT).has_value());
+    REQUIRE(base.Get<cfg::Vec3d>(ATTRIBUTES_POINT).value() == e_point_xyz);
+    REQUIRE(base.Get<cfg::Vec3i>(ATTRIBUTES_RGB).has_value());
+    REQUIRE(base.Get<cfg::Vec3i>(ATTRIBUTES_RGB).value() == e_rgb);
+    REQUIRE(base.Get<cfg::Vec3b>(ATTRIBUTES_FLAGS).has_value());
+    REQUIRE(base.Get<cfg::Vec3b>(ATTRIBUTES_FLAGS).value() == e_flags);
+    REQUIRE(base.Get<cfg::Vec3Str>(ATTRIBUTES_NAMES).has_value());
+    REQUIRE(base.Get<cfg::Vec3Str>(ATTRIBUTES_NAMES).value() == e_names);
 }
